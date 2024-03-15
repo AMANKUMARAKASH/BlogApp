2a0bf38f8a89a295f25b7089564e155073ce5ba8
AMANKUMARAKASH <89243224+AMANKUMARAKASH@users.noreply.github.com>
2024-03-15 11:36:34 +0530

    changes made

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,4 +1,4 @@
-import LoginForm from "@/components/loginForm/loginForm";
+import LoginForm from "@/components/loginForm/LoginForm";
 import styles from "./login.module.css";
 import { handleGithubLogin } from "@/lib/action";
 
