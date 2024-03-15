2ad39b739170c57cd62f99f65333e5a31e20bb88
AMANKUMARAKASH <89243224+AMANKUMARAKASH@users.noreply.github.com>
2024-03-15 13:50:24 +0530

    new deployment

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -8,7 +8,7 @@ import { getPost } from "@/lib/data";
 // FETCH DATA WITH AN API
 const getData = async (slug) => {
   try {
-    const res = await fetch(`https://blog-next-aman.netlify.app/api/blog/${slug}`);
+    const res = await fetch(`https://blog-aman-nine.vercel.app/api/blog/${slug}`);
 
     if (!res.ok) {
       const errorData = await res.json();
